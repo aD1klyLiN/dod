9974987a0779a1cdeaf59385d4d10a18ab6d560a
Anton <asmxajk@live.ru>
2016-06-06 20:04:02 +0300

    Initial commit

---
+++ app/src/main/java/pes/dungeonofdoom/acDod.java
@@ -0,0 +1,12 @@
+package pes.dungeonofdoom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class acDod extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
