bfa73fbed685fd5d661576d17617eedf4748e13a
Anton <asmxajk@live.ru>
2016-06-08 09:12:32 +0300

    Урок 142. Рисование 3

--- app/src/main/java/pes/dungeonofdoom/acIntro.java
+++ app/src/main/java/pes/dungeonofdoom/acIntro.java
@@ -107,70 +107,35 @@ public class acIntro extends AppCompatActivity {
 
             public void drawFigs(Canvas canvas) {
                 Paint p = new Paint();
-                RectF rectf = new RectF(700,100,800,150);
-                float[] points = new float[]{100,50,150,100,150,200,50,200,50,100};
-                float[] points1 = new float[]{300,200,600,200,300,300,600,300,400,100,400,400,500,100,500,400};
+                Rect rect = new Rect(100,200,200,300);
+                StringBuilder sb = new StringBuilder();
+
                 canvas.drawARGB(80, 102, 204, 255);
 
-                p.setColor(Color.RED);
+                p.setColor(Color.BLUE);
                 p.setStrokeWidth(10);
 
-                // рисуем точки их массива points
-                canvas.drawPoints(points,p);
-
-                // рисуем линии по точкам из массива points1
-                canvas.drawLines(points1,p);
-
-                // перенастраиваем кисть на зеленый цвет
-                p.setColor(Color.GREEN);
-
-                // рисуем закругленный прямоугольник по координатам из rectf
-                // радиусы закругления = 20
-                canvas.drawRoundRect(rectf, 20, 20, p);
-
-                // смещаем коорднаты rectf на 150 вниз
-                rectf.offset(0, 150);
-                // рисуем овал внутри прямоугольника rectf
-                canvas.drawOval(rectf, p);
-
-                // смещаем rectf в (900,100) (левая верхняя точка)
-                rectf.offsetTo(900, 100);
-                // увеличиваем rectf по вертикали на 25 вниз и вверх
-                rectf.inset(0, -25);
-                // рисуем дугу внутри прямоугольника rectf
-                // с началом в 90, и длиной 270
-                // соединение крайних точек через центр
-                canvas.drawArc(rectf, 90, 270, true, p);
-
-                // смещаем коорднаты rectf на 150 вниз
-                rectf.offset(0, 150);
-                // рисуем дугу внутри прямоугольника rectf
-                // с началом в 90, и длиной 270
-                // соединение крайних точек напрямую
-                canvas.drawArc(rectf, 90, 270, false, p);
-
-                // перенастраиваем кисть на толщину линии = 3
-                p.setStrokeWidth(3);
-                // рисуем линию (150,450) - (150,600)
-                canvas.drawLine(150, 450, 150, 600, p);
-
-                // перенастраиваем кисть на синий цвет
-                p.setColor(Color.BLUE);
-
-                // настраиваем размер текста = 30
                 p.setTextSize(30);
-                // рисуем текст в точке (150,500)
-                canvas.drawText("text left", 150, 500, p);
 
-                // настраиваем выравнивание текста на центр
-                p.setTextAlign(Paint.Align.CENTER);
-                // рисуем текст в точке (150,525)
-                canvas.drawText("text center", 150, 525, p);
+                // создаем строку с значениями ширины и высоты канвы
+                sb.setLength(0);
+                sb.append("width = ").append(canvas.getWidth())
+                        .append(", height = ").append(canvas.getHeight());
+                canvas.drawText(sb.toString(), 100, 100, p);
 
-                // настраиваем выравнивание текста на левое
-                p.setTextAlign(Paint.Align.RIGHT);
-                // рисуем текст в точке (150,550)
-                canvas.drawText("text right", 150, 550, p);
+                // перенастраивам кисть на заливку
+                p.setStyle(Paint.Style.FILL);
+                canvas.drawRect(rect, p);
+
+                // перенастраивам кисть на контуры
+                p.setStyle(Paint.Style.STROKE);
+                rect.offset(150, 0);
+                canvas.drawRect(rect, p);
+
+                // перенастраивам кисть на заливку + контуры
+                p.setStyle(Paint.Style.FILL_AND_STROKE);
+                rect.offset(150, 0);
+                canvas.drawRect(rect, p);
             }
         }
     }
