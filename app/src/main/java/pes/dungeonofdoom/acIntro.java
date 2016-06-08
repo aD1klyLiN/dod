3811623be243ef1f108ab7a359d6a53eecbbb751
Anton <asmxajk@live.ru>
2016-06-08 09:08:33 +0300

    Урок 142. Рисование 2

--- app/src/main/java/pes/dungeonofdoom/acIntro.java
+++ app/src/main/java/pes/dungeonofdoom/acIntro.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.SurfaceHolder;
@@ -104,36 +105,72 @@ public class acIntro extends AppCompatActivity {
                 }
             }
 
-            public void drawFigs(Canvas canv) {
+            public void drawFigs(Canvas canvas) {
                 Paint p = new Paint();
-                Rect rect = new Rect();
-                // заливка канвы цветом
-                canv.drawARGB(80, 102, 204, 255);
+                RectF rectf = new RectF(700,100,800,150);
+                float[] points = new float[]{100,50,150,100,150,200,50,200,50,100};
+                float[] points1 = new float[]{300,200,600,200,300,300,600,300,400,100,400,400,500,100,500,400};
+                canvas.drawARGB(80, 102, 204, 255);
 
-                // настройка кисти
-                // красный цвет
                 p.setColor(Color.RED);
-                // толщина линии = 10
                 p.setStrokeWidth(10);
 
-                // рисуем точку (50,50)
-                canv.drawPoint(50, 50, p);
+                // рисуем точки их массива points
+                canvas.drawPoints(points,p);
 
-                // рисуем линию от (100,100) до (500,50)
-                canv.drawLine(100,100,500,50,p);
+                // рисуем линии по точкам из массива points1
+                canvas.drawLines(points1,p);
 
-                // рисуем круг с центром в (100,200), радиус = 50
-                canv.drawCircle(100, 200, 50, p);
+                // перенастраиваем кисть на зеленый цвет
+                p.setColor(Color.GREEN);
 
-                // рисуем прямоугольник
-                // левая верхняя точка (200,150), нижняя правая (400,200)
-                canv.drawRect(200, 150, 400, 200, p);
+                // рисуем закругленный прямоугольник по координатам из rectf
+                // радиусы закругления = 20
+                canvas.drawRoundRect(rectf, 20, 20, p);
 
-                // настройка объекта Rect
-                // левая верхняя точка (250,300), нижняя правая (350,500)
-                rect.set(250, 300, 350, 500);
-                // рисуем прямоугольник из объекта rect
-                canv.drawRect(rect, p);
+                // смещаем коорднаты rectf на 150 вниз
+                rectf.offset(0, 150);
+                // рисуем овал внутри прямоугольника rectf
+                canvas.drawOval(rectf, p);
+
+                // смещаем rectf в (900,100) (левая верхняя точка)
+                rectf.offsetTo(900, 100);
+                // увеличиваем rectf по вертикали на 25 вниз и вверх
+                rectf.inset(0, -25);
+                // рисуем дугу внутри прямоугольника rectf
+                // с началом в 90, и длиной 270
+                // соединение крайних точек через центр
+                canvas.drawArc(rectf, 90, 270, true, p);
+
+                // смещаем коорднаты rectf на 150 вниз
+                rectf.offset(0, 150);
+                // рисуем дугу внутри прямоугольника rectf
+                // с началом в 90, и длиной 270
+                // соединение крайних точек напрямую
+                canvas.drawArc(rectf, 90, 270, false, p);
+
+                // перенастраиваем кисть на толщину линии = 3
+                p.setStrokeWidth(3);
+                // рисуем линию (150,450) - (150,600)
+                canvas.drawLine(150, 450, 150, 600, p);
+
+                // перенастраиваем кисть на синий цвет
+                p.setColor(Color.BLUE);
+
+                // настраиваем размер текста = 30
+                p.setTextSize(30);
+                // рисуем текст в точке (150,500)
+                canvas.drawText("text left", 150, 500, p);
+
+                // настраиваем выравнивание текста на центр
+                p.setTextAlign(Paint.Align.CENTER);
+                // рисуем текст в точке (150,525)
+                canvas.drawText("text center", 150, 525, p);
+
+                // настраиваем выравнивание текста на левое
+                p.setTextAlign(Paint.Align.RIGHT);
+                // рисуем текст в точке (150,550)
+                canvas.drawText("text right", 150, 550, p);
             }
         }
     }
